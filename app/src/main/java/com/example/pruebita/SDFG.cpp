c4b328911e1dc326f2f971f2c445f31eb1f9f3dd
dragonDercein <drakogo86@gmail.com>
2020-11-23 13:29:32 -0400

    SEGUNDA

---
+++ app/src/main/java/com/example/pruebita/SDFG.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dercein on 23/11/2020.
+//
+
+#include "SDFG.h"
---
+++ app/src/main/java/com/example/pruebita/SDFG.h
@@ -0,0 +1,16 @@
+//
+// Created by Dercein on 23/11/2020.
+//
+
+#ifndef PRUEBITA_SDFG_H
+#define PRUEBITA_SDFG_H
+
+
+
+class SDFG {
+
+};
+
+
+
+#endif //PRUEBITA_SDFG_H
